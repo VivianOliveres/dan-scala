a5c9991b66aceb3a53f6c87ba81d8d1e57bb6319
Vivian Oliveres <v.oliveres@gmail.com>
2017-05-26 12:24:17 +0200

    part_2_collections -> e7_option

--- src/test/scala/part_2_collections/e7_option.scala
+++ src/test/scala/part_2_collections/e7_option.scala
@@ -13,28 +13,28 @@ import support.HandsOnSuite
 class e7_option extends HandsOnSuite {
 
   exercice("None is...None") {
-   None should be(__)
+   None should be(None)
   }
 
   exercice("None is the empty Option") {
-    None.isEmpty should be(__)
+    None.isEmpty should be(true)
 
     val optional: Option[String] = None
-    optional.isEmpty should be(__)
-    optional should be(__)
+    optional.isEmpty should be(true)
+    optional should be(None)
   }
 
   exercice("Some is the filled Option") {
     val optional: Option[String] = Some("Some Value")
-    (optional == None) should be(__)
-    optional.isEmpty should be(__)
+    (optional == None) should be(false)
+    optional.isEmpty should be(false)
   }
 
   exercice("Option.getOrElse can be used to extract the Option value, by providing a default value if the Option is empty") {
     val optional: Option[String] = Some("Some Value")
     val optional2: Option[String] = None
-    optional.getOrElse("No Value") should be(__)
-    optional2.getOrElse("No Value") should be(__)
+    optional.getOrElse("No Value") should be("Some Value")
+    optional2.getOrElse("No Value") should be("No Value")
   }
 
 }
