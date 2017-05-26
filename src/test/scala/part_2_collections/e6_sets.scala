9c7ba5d7b00cb0f9b8363659807555a1c2925cec
Vivian Oliveres <v.oliveres@gmail.com>
2017-05-26 12:21:15 +0200

    part_2_collections -> e6_sets

--- src/test/scala/part_2_collections/e6_sets.scala
+++ src/test/scala/part_2_collections/e6_sets.scala
@@ -17,10 +17,10 @@ class e6_sets extends HandsOnSuite {
   */
   exercice("Creating a Set") {
     val mySet = Set("Sud", "Est", "Ouest", "Nord")
-    mySet.size should be(__)
+    mySet.size should be(4)
 
     val myOtherSet = Set("Sud", "Est", "Sud", "Nord")
-    myOtherSet.size should be(__)
+    myOtherSet.size should be(3)
   }
 
   /**
@@ -31,19 +31,19 @@ class e6_sets extends HandsOnSuite {
     val mySet = Set("Sud", "Est", "Sud")
     val aNewSet = mySet + "Nord"
 
-    aNewSet.contains("Nord") should be(__)
+    aNewSet.contains("Nord") should be(true)
 
     // Suppression
     val mySetBis = Set("Sud", "Est", "Ouest", "Nord")
     val aNewSetBis = mySetBis - "Nord"
     // method contains
-    aNewSetBis.contains("Nord") should be(__)
+    aNewSetBis.contains("Nord") should be(false)
 
     // Multiple suppressions
     val myOtherSet = Set("Sud", "Est", "Ouest", "Nord")
     val aNewOtherSet = myOtherSet -- List("Ouest", "Nord")
 
-    aNewOtherSet.contains("Nord") should be(__)
-    aNewOtherSet.contains("Ouest") should be(__)
+    aNewOtherSet.contains("Nord") should be(false)
+    aNewOtherSet.contains("Ouest") should be(false)
   }
 }
